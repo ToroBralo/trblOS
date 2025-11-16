a2467c67533c5835adc610060168dfe373f62e98
ToroBralo <torobralo2012@gmail.com>
2025-11-17 00:31:30 +0100

    added vga buffer

--- trbl-os/src/main.rs
+++ trbl-os/src/main.rs
@@ -3,6 +3,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -10,19 +11,13 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"TOROBRALO IS HERE.";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
 
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::print_something();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[warn(clippy::empty_loop)]
 
     loop{}
 
